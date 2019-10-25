fa07957cc3407902216863afab2d3b17e1f3055a
Hugh Sturrock <hughsturrock@gmail.com>
2019-10-25 11:50:40 -0700

    Adding space_time_ppmify functions

---
+++ R/space_time_ppmify.R
@@ -0,0 +1,149 @@
+#' The space_time_ppmify function
+#'
+#' This function builds off the ppmify function from Nick Golding's ppmify package, 
+#' converting your case data into a data.frame suitable for applying Poisson regression models.
+#' @param points sfc object with a `Date` associated with the point in yyyy-mm-dd format 
+#' @param layer_name Optional. Names of the bioclimatic/environmental layers to use as covariates. 
+#' Currently only uses static covariates. 
+#' See [here under 'Layer names'](https://github.com/disarm-platform/fn-covariate-extractor/blob/master/SPECS.md) 
+#' for a list of options. If none provided, spatial only model is assumed. 
+#' @param exposure rasterLayer of exposure (to be used as offset). Required. 
+#' Raster representing the population over which points arose. C
+#' urrently only accepts a single raster which is used across all time periods.
+#' @param resolution Resolution in km2 that covariates and offset should be resampled to (>= 1km2). Defaults to 1. 
+#' @param date_start_end Required. Vector of 2 values representing the start and end 
+#' times over which points were observed in yyyy-mm-dd format
+#' @param num_periods Number of time periods over which to aggregate points, 
+#' where 1 considers all points in a single time period 
+#' (equivalent to assuming a spatial only model). Defaults to 1.
+#' @param density Required. Density of quadrature points to generate (points / square km)
+#' @param prediction_frame Logical. Whether the function should also return a 
+#' data frame required for prediction (i.e. raster cells)? Defaults to FALSE.
+#' @param prediction_exposure Optional rasterLayer of exposure to be used for prediction. 
+#' This may be different to `exposure` if `points` arose from a different population 
+#' to that you wish to predict to. Automatically resampled to the same 
+#' resolution as `exposure`. Defaults to `exposure`. 
+#' @return A data.frame object with the following fields:
+#' \itemize{
+#'   \item x - x coordinates
+#'   \item y - y coordinates
+#'   \item exposure - Population for that point in space and time to be used as offset (when logged)
+#'   \item period - Number 1 through number of layers as determined by `date_start_end` and `num_periods`
+#'   \item outcome - Whether the point is an observation (1) or a quadrature point (0)
+#'   \item regression_weights - Number of `outcomes` per space-time cell, to be used as a regression weight
+#' }
+#' @export
+#' @import ppmify httr raster sf sp
+#' @examples 
+
+space_time_ppmify <- function(points,
+                              layer_name=NULL,
+                              exposure,
+                              resolution=1,
+                              date_start_end, 
+                              num_periods=1,
+                              prediction_exposure,
+                              density,
+                              prediction_frame=FALSE) {
+  
+  # run function and catch result
+  exposure_raster <- exposure
+  prediction_exposure_raster <- prediction_exposure
+  prediction_exposure_raster <- raster::resample(prediction_exposure_raster, exposure_raster)
+  reference_raster <- exposure_raster # TODO - allow this to be controlled as parameter in function when exposure not provided using boundary and resolution
+  points_coords <- st_coordinates(points)
+  
+  # Make ppmify object
+  ppmx <- ppmify::ppmify(points_coords, 
+                         area = exposure_raster,
+                         #covariates = exposure_raster, 
+                         density = density, #TODO change to be automatic
+                         method = "grid")
+  
+  # Aggregate points in space/time (i.e. aggregate points in same pixel)
+  ppm_cases_points_counts <- aggregate_points_space_time(points, ppmx, num_periods, date_start_end, reference_raster)
+  
+  # Make these population extractions the weights
+  ppm_cases_points_counts$exposure <- raster::extract(exposure_raster,
+                                                      cbind(ppm_cases_points_counts$x, ppm_cases_points_counts$y))
+  
+  # If an exposure (population) is provided, change the weights to be scaled by population
+  if(!is.null(exposure)){
+    ppm_int_points_period <- get_int_points_exposure_weights(ppmx, ppm_cases_points_counts,
+                                                             exposure_raster, num_periods)
+  }
+  
+  # add model month column for integration points (already generated as 'month')
+  ppm_df <- rbind(ppm_cases_points_counts, ppm_int_points_period)
+  
+  # add column of 1's to cells with cases and 0's to cells without
+  # this will act as your outcome in the poisson model
+  ppm_df$outcome <- ifelse(ppm_df$points > 0, 1, 0)
+  
+  # change any 0's in the points column to 1
+  # this will act as regression weights in the poisson model
+  ppm_df$regression_weights <- ppm_df$points
+  ppm_df$regression_weights[ppm_df$regression_weights == 0] <- 1
+  
+  # divide the exposure by the number of cases in each cell
+  ppm_df$weights <- ppm_df$weights/ppm_df$regression_weights
+  
+  # Get covariate values at data and prediction points
+  ##### AT data points
+  ppm_df_sf <- st_as_sf(SpatialPoints(ppm_df[,c("x", "y")]))
+  input_data_list <- list(
+    points = geojson_list(ppm_df_sf),
+    layer_names = layer_name,
+    resolution = resolution
+  )
+  
+  response <-
+    httr::POST(
+      url = "https://faas.srv.disarm.io/function/fn-covariate-extractor",
+      body = as.json(input_data_list),
+      content_type_json(),
+      timeout(300)
+    )
+  
+  response_content <- content(response)
+  ppm_df_sf_with_covar <- st_read(as.json(response_content$result), quiet = TRUE)
+  
+  # Merge with ppm_df 
+  ppm_df <- cbind(ppm_df, as.data.frame(ppm_df_sf_with_covar))
+  
+  # Drop unnessecary columns
+  ppm_df <- subset(ppm_df, select=-c(weights, points, geometry))
+  
+  if(prediction_frame==FALSE){
+    return(list(ppm_df = ppm_df))
+  }else{
+    
+    ##### At prediction points
+    pred_point_coords <- coordinates(reference_raster)[which(!is.na(reference_raster[])),]
+    pred_points_sf <- st_as_sf(SpatialPoints(pred_point_coords))
+    input_data_list_pred_points <- list(
+      points = geojson_list(pred_points_sf),
+      layer_names = layer_name
+    )
+    
+    response_pred_points <-
+      httr::POST(
+        url = "https://faas.srv.disarm.io/function/fn-covariate-extractor",
+        body = as.json(input_data_list_pred_points),
+        content_type_json(),
+        timeout(300)
+      )
+    
+    response_content_pred_points <- content(response_pred_points)
+    pred_points_with_covar <- st_read(as.json(response_content_pred_points$result), quiet = TRUE)
+    ppm_df_pred <- cbind(pred_points_with_covar, pred_point_coords)
+    ppm_df_pred$exposure <- prediction_exposure_raster[which(!is.na(exposure_raster[]))]
+    
+    # Drop unnessecary columns
+    ppm_df_pred <- as.data.frame(ppm_df_pred)
+    ppm_df_pred <- subset(ppm_df_pred, select=-c(geometry))
+    
+    return(list(ppm_df = ppm_df,
+                ppm_df_pred = ppm_df_pred))
+  }
+}
---
+++ R/space_time_ppmify_helpers.R
@@ -0,0 +1,109 @@
+#' The get_int_points_exposure_weights function
+#'
+#' Helper function for space_time_ppmify
+#' @param ppmx 
+#' @param ppm_cases_points_counts
+#' @param exposure_raster
+#' @param num_periods
+
+get_int_points_exposure_weights <- function(ppmx, ppm_cases_points_counts, exposure_raster, num_periods){
+
+  # First identify which are the local_cases and integration rows
+  # in the ppm object
+  ppm_int_points <- ppmx[ppmx$points==0,]
+
+  # extract population within each voronoi polygon around each integration point
+  # First remove any pixels with cases as you need to estimate population in pixels without cases
+  dd <- deldir::deldir(ppm_int_points$x, ppm_int_points$y)
+  tiles <- deldir::tile.list(dd)
+
+  
+  polys <- vector(mode = "list", length = length(tiles))
+  for (i in seq(along = polys)) {
+    pcrds <- cbind(tiles[[i]]$x, tiles[[i]]$y)
+    pcrds <- rbind(pcrds, pcrds[1,])
+    polys[[i]] <- sp::Polygons(list(sp::Polygon(pcrds)), ID = as.character(i))
+  }
+  spoly <- sp::SpatialPolygons(polys)
+  
+  ppm_int_points_period <- NULL
+  for(j in 1:num_periods){
+    exposure_raster_non_case_pixels <- exposure_raster
+    ppm_case_points_coords <- ppm_cases_points_counts[ppm_cases_points_counts$period == j, c("x", "y")]
+    exposure_raster_non_case_pixels[raster::cellFromXY(exposure_raster_non_case_pixels,
+                                                    ppm_case_points_coords)] <- NA
+
+  # Extract from offset raster
+  exposure_raster_velox <- velox(exposure_raster_non_case_pixels)
+  ppm_int_points$exposure <- exposure_raster_velox$extract(spoly, fun = function(x){sum(x, na.rm = TRUE)})
+  
+  # bind with previous periods
+    ppm_int_points$period <- j
+    ppm_int_points_period <- rbind(ppm_int_points_period, ppm_int_points)
+  }
+  
+  # Remove any points with 0 offset
+  ppm_int_points_period <- ppm_int_points_period[-which(ppm_int_points_period$exposure <= 0),]
+  
+  return(ppm_int_points_period)
+}  
+
+
+#' The aggregate_points_space_time function
+#'
+#' Helper function for space_time_ppmify
+#' @param points
+#' @param ppmx
+#' @param num_periods
+#' @param date_start_end
+#' @param reference_raster
+
+
+# Deal with case points
+# First aggregate any cases occuring in the same pixel in the same month
+# # loop through each month and identify any cases in the same pixel
+aggregate_points_space_time <- function(points, ppmx, num_periods, date_start_end, reference_raster){
+
+      ppm_cases_points <- ppmx[ppmx$points==1,]
+      ppm_cases_points_counts <- ppm_cases_points[FALSE,]
+      
+      # Define date breaks
+      dates <- seq(ymd(date_start_end[1]), ymd(date_start_end[2]), 1)
+      date_breaks <- c(levels(cut.Date(dates, num_periods, right=TRUE, include.lowest = TRUE)),
+                       date_start_end[2])
+      browser()
+      for(i in 1:num_periods){
+        
+        cases_model_period <- as.numeric(cut.Date(ymd(points$date), ymd(date_breaks)))
+        cases_period <- ppm_cases_points[cases_model_period==i,]
+        cases_period$period <- i
+        
+        if(nrow(cases_period)>0){
+          
+          # calculate which cell each case is in
+          cases_period$case_pixel <- raster::cellFromXY(reference_raster,
+                                                       cbind(cases_period$x, cases_period$y))
+          
+          # Aggregate number of cases by cell
+          cell_counts <- raster::aggregate(cases_period$case_pixel,
+                                           by = list(cases_period$case_pixel), length)
+          
+          # create aggregated ppm data frame for aggregated case counts
+          cases_period_trimmed <- cases_period[match(cell_counts$Group.1, cases_period$case_pixel),]
+          
+          # Aggregate case coordinates to be the centroid of the pixel
+          cases_period_trimmed[,c("x","y")] <- sp::coordinates(reference_raster)[cases_period_trimmed$case_pixel,]
+          
+          # change number of points to be aggregate number
+          cases_period_trimmed$points <- cell_counts$x
+          
+          # take the columns you need
+          cases_period_trimmed<- cases_period_trimmed[, 1:5]
+          #names(cases_period_trimmed) <- names(ppm_cases_points)
+          
+          # bind to complete the loop
+          ppm_cases_points_counts <- rbind(ppm_cases_points_counts, cases_period_trimmed)
+        }
+      }
+      return(ppm_cases_points_counts)
+}
