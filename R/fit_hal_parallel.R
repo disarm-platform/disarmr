f26d66c67b84680612b546c8df1310ae90013c87
Hugh Sturrock <hughsturrock@gmail.com>
2019-12-11 20:56:03 -0800

    Removing hal9001 from fit_hal_parallel

--- R/fit_hal_parallel.R
+++ R/fit_hal_parallel.R
@@ -5,7 +5,7 @@
 #' @param n_pos_var Name of column corresponding to numbers positive
 #' @param n_neg_var Name of column corresponding to numbers negative
 #' @export
-library(hal9001)
+#library(hal9001)
 fit_hal_parallel <- function(folds_df_list_fold,
                              X_var,
                              n_pos_var,
